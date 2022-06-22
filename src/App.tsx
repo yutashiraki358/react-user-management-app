3ae475997d2803058dff1dab93197ec2286cfda5
Yuta Shiraki <yuta.shiraki358@gmail.com>
2022-06-22 22:52:20 +0900

    materialUi-test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <div className="App">
       <h1>ユーザー管理アプリ</h1>
+      <Button variant="contained">Contained</Button>
     </div>
   );
 }
