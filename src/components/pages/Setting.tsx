9d5360274e5ada31ad96dd19f77c3a5004f4eb04
Yuta Shiraki <yuta.shiraki358@gmail.com>
2022-06-28 11:43:16 +0900

    ヘッダーの作成

--- src/components/pages/Setting.tsx
+++ src/components/pages/Setting.tsx
@@ -1,5 +1,11 @@
 import { memo, VFC } from "react";
+import Button from "@mui/material/Button";
 
 export const Setting: VFC = memo(() => {
-  return <p>設定ページ</p>;
+  return (
+    <>
+      <p>設定ページ</p>
+      <Button variant="contained">設定完了</Button>
+    </>
+  );
 });
